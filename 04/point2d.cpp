bf8ab7236a704379b021d82fa6dd440c66ebb0bb
José Proença <jose@proenca.org>
2025-02-24 14:10:34 +0000

    overloading operator in point2d; typo fix

--- 04/point2d.cpp
+++ 04/point2d.cpp
@@ -6,6 +6,7 @@ struct point2d {
   double y;
 };
 
+// s1: return new point
 point2d sum(point2d a, point2d b) {
   point2d r;
   r.x = a.x + b.x;
@@ -13,19 +14,35 @@ point2d sum(point2d a, point2d b) {
   return r;
 }
 
+// s2: update references
 void sum(const point2d& a, const point2d& b, point2d& r) {
   r.x = a.x + b.x;
   r.y = a.y + b.y;
 }
 
+// s3: overloading "+" (not in the slides):
+point2d operator+(const point2d &a, const point2d &b) {
+  return {a.x+b.x, a.y+b.y};
+}
+
 int main() {
+  int a = 0xa, b = 20, c = 30;
+  auto d = b + c + 'x';
+
+  char a2 = 'x';
+  short b2 = 2;
+  double a2 = 3.5;
+  int d2 = b2 + 4;
+
+
   std::cout << "sizeof(point2d): " << sizeof(point2d) << '\n';
  
   point2d a { 1.2, 3.4 };
   std::cout << "a: " << a.x << ' ' << a.y << '\n';
 
-  point2d b = a;
-  std::cout << "b: " << a.x << ' ' << b.y << '\n';
+  point2d& b = a;
+  b.x = 2.2;
+  std::cout << "b: " << b.x << ' ' << b.y << '\n';
 
   point2d s1 = sum(a,b);
   std::cout << "s1: " << s1.x << ' ' << s1.y << '\n';
@@ -33,6 +50,9 @@ int main() {
   point2d s2; 
   sum(a, b, s2);
   std::cout << "s2: " << s2.x << ' ' << s2.y << '\n';
+
+  point2d s3 = a + b; 
+  std::cout << "s3: " << s3.x << ' ' << s3.y << '\n';
    
   return 0;
 }
